f831eaa1883d7381fae55be357db137b588f653e
John Kang <pollyfactor@hotmail.com>
2020-03-06 18:56:05 -0500

    added comments for script.js

--- script.js
+++ script.js
@@ -1,17 +1,19 @@
 // Assignment Code
+
+//empty array based on user choices
 var customArr = [];
+//prompt and confirm arrays
 var lowerArr = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
 var upperArr = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","W","X","Y","Z"];
 var numArr = ["1","2","3","4","5","6","7","8","9","0"];
 var specArr = ["!","@","#","$","%","^","&","*","(",")","{","}","|","[","]",";","'",":","<",">","?","/"];
 
-var userPass;
-
+//main function
 function passGen() {
-
+//initial prompt
 var charInput = parseInt(prompt("How many characters in the password? Choose a number between 8 and 128."));
 
-
+//conditions if user enters wrong inputs
     if (isNaN(charInput)) {
         alert("Please enter a number between 8 and 128.");
         passGen();
@@ -19,17 +21,18 @@ var charInput = parseInt(prompt("How many characters in the password? Choose a n
         alert("Please enter a number between 8 and 128.");
         passGen();
     } else {
+//checking on boolean of inputs
       var lowerInput = confirm("Lowercase letters Ok?");
       var upperInput = confirm("Uppercase letters Ok?");
       var numInput = confirm("Numbers Ok?");
       var specInput = confirm("Special characters Ok?");  
     }
-  
+//condition if user chooses 'cancel' on all confirms'
     if (!lowerInput && !upperInput && !numInput && !specInput) {
       alert("Please enter more criteria.");
       passGen();
     }
-    
+//pushes all 'ok' responses to a master array
     if (lowerInput == true){
     customArr.push(lowerArr);
     }
@@ -43,20 +46,7 @@ var charInput = parseInt(prompt("How many characters in the password? Choose a n
     customArr.push(specArr);
     }
 }
-
-for (var i = 0; i < charLength; i++){
-
-
-var generateBtn = document.querySelector("#generate");
-
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
+//running a loop based on number of characters
+for (var i = 0; i < charInput; i++){
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
