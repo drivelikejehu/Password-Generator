053954181886bf9735650fdf2c50420788f00ce5
John Kang <pollyfactor@hotmail.com>
2020-03-06 18:16:35 -0500

    updated functions in script.js

--- script.js
+++ script.js
@@ -3,7 +3,48 @@ var customArr = [];
 var lowerArr = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
 var upperArr = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","W","X","Y","Z"];
 var numArr = ["1","2","3","4","5","6","7","8","9","0"];
-var specCharArr = ["!","@","#","$","%","^","&","*","(",")","{","}","|","[","]",";","'",":","<",">","?","/"];
+var specArr = ["!","@","#","$","%","^","&","*","(",")","{","}","|","[","]",";","'",":","<",">","?","/"];
+
+var userPass;
+
+function passGen() {
+
+var charInput = parseInt(prompt("How many characters in the password? Choose a number between 8 and 128."));
+
+
+    if (isNaN(charInput)) {
+        alert("Please enter a number between 8 and 128.");
+        passGen();
+    } else if (charInput < 8 || charInput > 128) {
+        alert("Please enter a number between 8 and 128.");
+        passGen();
+    } else {
+      var lowerInput = confirm("Lowercase letters Ok?");
+      var upperInput = confirm("Uppercase letters Ok?");
+      var numInput = confirm("Numbers Ok?");
+      var specInput = confirm("Special characters Ok?");  
+    }
+  
+    if (!lowerInput && !upperInput && !numInput && !specInput) {
+      alert("Please enter more criteria.");
+      passGen();
+    }
+    
+    if (lowerInput == true){
+    customArr.push(lowerArr);
+    }
+    if (upperInput == true){
+    customArr.push(upperArr);
+    }
+    if (numInput == true){
+    customArr.push(numArr);
+    }
+    if (specInput == true){
+    customArr.push(specArr);
+    }
+}
+
+
 
 var generateBtn = document.querySelector("#generate");
 
