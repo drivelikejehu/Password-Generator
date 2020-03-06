b7b122a3a37d29ec77b0d73afd8247ee3c64b34e
John Kang <pollyfactor@hotmail.com>
2020-03-06 10:33:31 -0500

    added variables to style.css

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
 // Assignment Code
+var customArr = [];
+var lowerArr = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+var upperArr = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","W","X","Y","Z"];
+var numArr = ["1","2","3","4","5","6","7","8","9","0"];
+var specCharArr = ["!","@","#","$","%","^","&","*","(",")","{","}","|","[","]",";","'",":","<",">","?","/"];
+
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
