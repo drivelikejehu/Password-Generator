c71034134e4ecf1d117351c6696ad06dbe2db4a1
John Kang <pollyfactor@hotmail.com>
2020-06-02 22:34:39 -0400

    added prompts for checking user inputs

--- script.js
+++ script.js
@@ -6,13 +6,41 @@ const randomFunctions = {
 };
 
 const generateBtn = document.querySelector("#generate");
-generateBtn.addEventListener("click", randomLower);
+generateBtn.addEventListener("click", generatePassword);
+
+function generatePassword() {
+  //prompt user for password length
+  var charInput = parseInt(
+    prompt(
+      "How many characters in the password? Choose a number between 8 and 128."
+    )
+  );
+
+  //conditions if user enters wrong inputs
+  if (isNaN(charInput)) {
+    alert("Please enter a number between 8 and 128.");
+    generatePassword();
+  } else if (charInput < 8 || charInput > 128) {
+    alert("Please enter a number between 8 and 128.");
+    generatePassword();
+  } else {
+    //checking on boolean of inputs
+    var lowerInput = confirm("Lowercase letters Ok?");
+    var upperInput = confirm("Uppercase letters Ok?");
+    var numInput = confirm("Numbers Ok?");
+    var specInput = confirm("Special characters Ok?");
+  }
+  //condition if user chooses 'cancel' on all confirms'
+  if (!lowerInput && !upperInput && !numInput && !specInput) {
+    alert("Please enter more criteria.");
+    generatePassword();
+  }
+}
 
 // Generator functions
 
 function randomLower() {
-  // return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
-  console.log('heya')
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
 }
 
 function randomUpper() {
