677993947f939c4efbdaa469aea0edba49fb6330
John Kang <pollyfactor@hotmail.com>
2020-06-02 22:03:02 -0400

    added event listener for button

--- script.js
+++ script.js
@@ -1,17 +1,29 @@
+const randomFunctions = {
+  lower: randomLower,
+  upper: randomUpper,
+  number: randomNum,
+  character: randomChar,
+};
+
+const generateBtn = document.querySelector("#generate");
+generateBtn.addEventListener("click", randomLower);
+
+// Generator functions
+
 function randomLower() {
-  return String.fromCharCode(Math.floor(Math.random() * 26) + 97)
+  // return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+  console.log('heya')
 }
 
 function randomUpper() {
-  return String.fromCharCode(Math.floor(Math.random() * 26) + 65)
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
 }
 
-function randomNumber() {
-  return String.fromCharCode(Math.floor(Math.random() * 10) + 48)
+function randomNum() {
+  return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
 }
 
 function randomChar() {
-  const symbols = '!@#$%^&*(){}|[];:<>?/';
-    return symbols[Math.floor(Math.random() * symbols.length)];
+  const symbols = "!@#$%^&*(){}|[];:<>?/";
+  return symbols[Math.floor(Math.random() * symbols.length)];
 }
-
