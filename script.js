01ccd5ddcd56f802febcd40d8e4f5414e816dd43
John Kang <pollyfactor@hotmail.com>
2020-06-02 23:34:18 -0400

    new array generated on user-defined inputs'

--- script.js
+++ script.js
@@ -5,39 +5,49 @@ const randomFunctions = {
   character: randomChar,
 };
 
-const generateBtn = document.getElementById('generate');
-const passwordEl = document.getElementById('password');
+const generateBtn = document.getElementById("generate");
+const passwordEl = document.getElementById("password");
 
-generateBtn.addEventListener('click', generatePassword);
+generateBtn.addEventListener("click", generatePassword);
 
 function generatePassword() {
+  let passwordToReturn = "";
   //prompt user for password length
   var passwordLength = parseInt(
     prompt(
-      'How many characters in the password? Choose a number between 8 and 128.'
+      "How many characters in the password? Choose a number between 8 and 128."
     )
   );
 
   //conditions if user enters wrong inputs
   if (isNaN(passwordLength)) {
-    alert('Please enter a number between 8 and 128.');
+    alert("Please enter a number between 8 and 128.");
     generatePassword();
   } else if (passwordLength < 8 || passwordLength > 128) {
-    alert('Please enter a number between 8 and 128.');
+    alert("Please enter a number between 8 and 128.");
     generatePassword();
   } else {
     //checking on boolean of inputs
-    var lowerInput = confirm('Lowercase letters Ok?');
-    var upperInput = confirm('Uppercase letters Ok?');
-    var numInput = confirm('Numbers Ok?');
-    var specInput = confirm('Special characters Ok?');
+    var lowerInput = confirm("Lowercase letters Ok?");
+    var upperInput = confirm("Uppercase letters Ok?");
+    var numInput = confirm("Numbers Ok?");
+    var specInput = confirm("Special characters Ok?");
   }
   //condition if user chooses 'cancel' on all confirms'
   if (!lowerInput && !upperInput && !numInput && !specInput) {
-    alert('Please enter more criteria.');
+    alert("Please enter more criteria.");
     generatePassword();
   }
-  console.log(lowerInput, upperInput, numInput, specInput);
+  //determine how many parameters the user wants
+  const typesCount = lowerInput + upperInput + numInput + specInput;
+  console.log('typesCount: ', typesCount)
+
+  const typesArr = [{lowerInput}, {upperInput}, {numInput}, {specInput}].filter(
+    //checks each item in the array for 'true' boolean values
+    item => Object.values(item)[0]
+  )
+  console.log('typesArr: ', typesArr)
+
 }
 
 // Generator functions
@@ -55,6 +65,6 @@ function randomNum() {
 }
 
 function randomChar() {
-  const symbols = '!@#$%^&*(){}|[];:<>?/';
+  const symbols = "!@#$%^&*(){}|[];:<>?/";
   return symbols[Math.floor(Math.random() * symbols.length)];
 }
