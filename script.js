1c9e0f98e4350e388fe4a8dff6a8960a57d2b0d9
John Kang <pollyfactor@hotmail.com>
2020-06-02 20:30:55 -0400

    functions for all paramaters work

--- script.js
+++ script.js
@@ -1,5 +1,25 @@
-function getRandomLower() {
-  return String.fromCharCode(54)
+function randomLower() {
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 97)
 }
 
-console.log(Math.floor(Math.random() * 26)) 
+console.log(randomLower());
+
+function randomUpper() {
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 65)
+}
+
+console.log(randomUpper());
+
+function randomNumber() {
+  return String.fromCharCode(Math.floor(Math.random() * 10) + 48)
+}
+
+console.log(randomNumber());
+
+function randomChar() {
+  const symbols = '!@#$%^&*(){}|[];:<>?/';
+  // for (i = 0; i < symbols.length; i++) {}
+    return symbols[Math.floor(Math.random() * symbols.length)]
+}
+
+console.log(randomChar());
